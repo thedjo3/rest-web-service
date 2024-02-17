b6a6bbed4788dffd7d50e7629f9a739a439ab180
Djan Daudov <djandaudovv@gmail.com>
2024-02-17 21:35:06 +0200

    Requst Media type versioning

--- src/main/java/com/restful/restwebservice/versioning/VersioningPersonController.java
+++ src/main/java/com/restful/restwebservice/versioning/VersioningPersonController.java
@@ -40,4 +40,17 @@ public class VersioningPersonController {
     public PersonV2 getSecondVersionOfPersonRequestHeader() {
         return new PersonV2(new Name("Bob", "Charlie"));
     }
+
+
+    // Request Media type versioning - Accept Header
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v1+json")
+    public PersonV1 getFirstVersionOfPersonAcceptHeader() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v2+json")
+    public PersonV2 getSecondVersionOfPersonAcceptHeader() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
 }
