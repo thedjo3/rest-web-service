dfd6786c6e1052fc6d3e2428732a240acc65481a
Djan Daudov <djandaudovv@gmail.com>
2024-02-17 21:00:08 +0200

    Request Parameter versioning

--- src/main/java/com/restful/restwebservice/versioning/VersioningPersonController.java
+++ src/main/java/com/restful/restwebservice/versioning/VersioningPersonController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VersioningPersonController {
 
+    // URI Versioning
     @GetMapping("/v1/person")
     public PersonV1 getFirstVersionOfPerson() {
         return new PersonV1("Bob Charlie");
@@ -13,6 +14,18 @@ public class VersioningPersonController {
 
     @GetMapping("/v2/person")
     public PersonV2 getSecondVersionOfPerson() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+
+    // Request Parameter versioning
+    @GetMapping(path = "/person", params = "version=1")
+    public PersonV1 getFirstVersionOfPersonRequestParameter() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path = "/person", params = "version=2")
+    public PersonV2 getSecondVersionOfPersonRequestParameter() {
         return new PersonV2(new Name("Bob", "Charlie"));
     }
 }
